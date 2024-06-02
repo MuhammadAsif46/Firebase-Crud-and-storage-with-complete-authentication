59998e3726bb0195dd3b0f6c0cbe2cb1820826aa
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-02 20:14:25 +0500

    added and modify pages folder

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import Profile from '../components/Profile'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Profile from "../components/Profile/Profile"
 
 const ProfilePage = () => {
-  return <Profile/>
-}
+  const navigate = useNavigate();
 
-export default ProfilePage
+  return (
+    <>
+      <div className="flex justify-between items-center p-4 bg-[#a2d2ff] ">
+        <h1 className="text-3xl text-white">
+          Crud operation with firebase firestore{" "}
+        </h1>
+        <button
+          onClick={() => navigate("/update-profile")}
+          className="bg-gray-400 font-bold text-white p-3 rounded-md hover:bg-gray-500"
+        >
+          Update Profile
+        </button>
+      </div>
+
+      <Profile />
+    </>
+  );
+};
+
+export default ProfilePage;
---
+++ src/pages/UpdateProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UpdateProfile from "../components/UpdateProfile/UpdateProfile";
+
+const UpdateProfilePage = () => {
+  return (
+    <>
+      <div className="p-4 bg-[#a2d2ff] ">
+        <h1 className="text-3xl text-white">Update Profile </h1>
+      </div>
+      <UpdateProfile />
+    </>
+  );
+};
+
+export default UpdateProfilePage;
